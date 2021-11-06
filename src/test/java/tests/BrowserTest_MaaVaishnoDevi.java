3706e8f0e75e896a4df9d44edebd00ee55a1fb42
Deo Prasad Shaw <deoshaw3@gmail.com>
2021-11-06 19:31:56 +0530

    Added browser test for mobile

---
+++ src/test/java/tests/BrowserTest_MaaVaishnoDevi.java
@@ -0,0 +1,47 @@
+package tests;
+
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class BrowserTest_MaaVaishnoDevi {
+	AndroidDriver<MobileElement> driver;
+	
+	@Test
+	void testPlanAYatra() throws Exception
+	{
+		
+		
+		//Device & Platform info
+		DesiredCapabilities cap=new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME,"Appium");
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME,"Galaxy M31");
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
+		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION,"11");	
+		cap.setCapability(MobileCapabilityType.BROWSER_NAME,"Chrome");
+		
+		
+		driver=new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+		driver.get("https://www.maavaishnodevi.org/index.aspx");
+		driver.findElement(By.xpath("//*[text()='Plan a Yatra']")).click();
+		Thread.sleep(2000);
+		
+		String text=driver.findElement(By.xpath("//strong[text()='Rest of India to Jammu']")).getText();
+		JavascriptExecutor js=(JavascriptExecutor)driver;
+		js.executeScript("alert(arguments[0])", text);
+		Thread.sleep(3000);
+		driver.switchTo().alert().accept();
+		Thread.sleep(1000);
+		driver.quit();
+		
+		
+	}
+
+}
