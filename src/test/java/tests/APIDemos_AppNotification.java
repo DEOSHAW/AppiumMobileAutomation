52a6a7aa865f902b6d02130c3d754ac485fe9970
Deo Prasad Shaw <deoshaw3@gmail.com>
2022-08-03 18:58:30 +0530

    Added Notification API Demos test

--- src/test/java/tests/APIDemos_AppNotification.java
+++ src/test/java/tests/APIDemos_AppNotification.java
@@ -4,7 +4,9 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.TouchAction;
@@ -13,12 +15,11 @@ import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.touch.offset.ElementOption;
 
-public class APIDemos_GameController {
+public class APIDemos_AppNotification {
 	AndroidDriver<AndroidElement> driver;
 	
-	
 	@Test
-	void testGameController() throws Exception
+	void testAppNotification() throws Exception
 	{
 		DesiredCapabilities cap=new DesiredCapabilities();
 		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
@@ -27,20 +28,21 @@ public class APIDemos_GameController {
 		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "12");
 		cap.setCapability("appPackage","io.appium.android.apis");
 		cap.setCapability("appActivity","io.appium.android.apis.ApiDemos");
+		
 		driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		TouchAction action=new TouchAction(driver);
-		AndroidElement Views=driver.findElement(By.xpath("//*[@text='Views']"));
-		action.tap(ElementOption.element(Views)).perform();
-		AndroidElement GC=driver.findElement(By.xpath("//*[@text='Game Controller Input']"));
-		action.tap(ElementOption.element(GC)).perform();
-		String GCText=driver.findElementById("io.appium.android.apis:id/description").getText();
-		System.out.println(GCText);
-		Thread.sleep(2000);
+		driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+		AndroidElement App=driver.findElementByAccessibilityId("App");
+		action.tap(ElementOption.element(App)).perform();
+		AndroidElement Notification=driver.findElement(By.xpath("//*[@text='Notification']"));
+		action.tap(ElementOption.element(Notification)).perform();
+		AndroidElement IncomingMessage=driver.findElement(By.xpath("//*[@text='IncomingMessage']"));
+		action.tap(ElementOption.element(IncomingMessage)).perform();
+		AndroidElement showAppNotification=driver.findElement(By.xpath("//*[@text='SHOW APP NOTIFICATION']"));
+		
+		Assert.assertEquals("SHOW APP NOTIFICATION", showAppNotification.getText());
+		Thread.sleep(2500);
 		driver.quit();
-		
-		
-		
 		
 		
 		
