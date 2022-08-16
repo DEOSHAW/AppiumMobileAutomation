86662fac7e4ee6ebd2c1e9b1b3f4478696fe551e
Deo Prasad Shaw <deoshaw3@gmail.com>
2022-08-16 19:33:10 +0530

    Updated gitignore

---
+++ src/test/java/tests/APIDemos_Accessibilty_CustomView.java
@@ -0,0 +1,38 @@
+package tests;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class APIDemos_Accessibilty_CustomView {
+	AndroidDriver<AndroidElement> driver;
+	
+	
+	@Test
+	void testCustomView() throws Exception
+	{
+		DesiredCapabilities cap=new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android");
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11");
+		cap.setCapability("appPackage","io.appium.android.apis");
+		cap.setCapability("appActivity","io.appium.android.apis.ApiDemos");
+		driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		driver.findElement(By.xpath("//*[@text='Accessibility']")).click();
+		driver.findElement(By.xpath("//*[@text='Custom View']")).click();
+		Thread.sleep(3000);
+		driver.quit();
+		
+	}
+
+}
