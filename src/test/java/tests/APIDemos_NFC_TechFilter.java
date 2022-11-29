61e774b8388b7598ef189a6dc057a4410c772899
Deo Prasad Shaw <deoshaw3@gmail.com>
2022-11-29 16:57:45 +0530

    Added API Demos new test

---
+++ src/test/java/tests/APIDemos_NFC_TechFilter.java
@@ -0,0 +1,41 @@
+package tests;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class APIDemos_NFC_TechFilter {
+	AndroidDriver<AndroidElement> driver;
+	
+	@Test
+	void testNFCTechFilter() throws Exception
+	{
+		
+		DesiredCapabilities cap=new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Galaxy");
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11");
+		cap.setCapability("appPackage","io.appium.android.apis");
+		cap.setCapability("appActivity","io.appium.android.apis.ApiDemos");
+		driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//*[@text='NFC']")).click();
+		driver.findElement(By.xpath("//*[@text='TechFilter']")).click();
+		
+		String Text=driver.findElement(By.id("io.appium.android.apis:id/text")).getText();
+		System.out.println(Text);
+		Thread.sleep(3000);
+		driver.quit();
+		
+	}
+
+}
