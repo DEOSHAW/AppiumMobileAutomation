23a8213cb8dbe51c7bfb9162f57f8147c8194c1b
Deo Prasad Shaw <deoshaw3@gmail.com>
2026-04-21 13:47:21 +0530

    Updated base class

--- src/main/java/Base/Baseclass.java
+++ src/main/java/Base/Baseclass.java
@@ -31,8 +31,14 @@ public class Baseclass {
 		
 		UiAutomator2Options options=new UiAutomator2Options();
 		//options.setApp(System.getProperty("user.dir")+File.separator+"Apps\\General-Store.apk");
-		options.setApp(System.getProperty("user.dir")+File.separator+"Apps\\ApiDemos-debug.apk");
+		//options.setApp(System.getProperty("user.dir")+File.separator+"Apps\\ApiDemos-debug.apk");
 		options.setDeviceName("emulator-5554");
+		//For Real device (You may remove above step to set app if app is already installed and instead use app package and app activity
+		//options.setDeviceName("192.168.29.149:45865");
+		//options.setUdid("192.168.29.149:45865"); // needed to avoid conflict if multiple connected devices
+		//options.setNoReset(true);
+		options.setAppPackage("io.appium.android.apis");
+		options.setAppActivity("io.appium.android.apis.ApiDemos");
 		options.setCapability("uiautomator2ServerLaunchTimeout", 60000);
 		options.setChromedriverExecutable(System.getProperty("user.dir")+File.separator+"chromedriver.exe");
 		driver=new AndroidDriver(new URL("http://127.0.0.1:4723"),options);
